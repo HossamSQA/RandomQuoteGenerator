545e6ce84f84d7747a8e5db4d4df55794fc5679f
Hossam1104 <Eng_hossam_mohamed@hotmail.com>
2022-02-19 15:11:39 +0200

    adding JS code

--- app.js
+++ app.js
@@ -3,4 +3,18 @@ generateBtn.addEventListener("click", getQuote);
 
 function getQuote(e) {
   e.preventDefault();
+
+  let quoteTxt = document.querySelector("#quote-text");
+  let authorName = document.querySelector("#quote-author");
+
+  fetch("https://type.fit/api/quotes")
+    .then((res) => res.json())
+
+    .then((data) => {
+      let quote = data[Math.floor(Math.random() * 1642)].text;
+      let author = data[Math.floor(Math.random() * 1642)].author;
+
+      quoteTxt.innerHTML = quote;
+      authorName.innerHTML = author;
+    });
 }
